cce70653e6bc8fc75a1369bb9ca0ccb9ac4ae1be
Kipp Williams <39743687+kipprr@users.noreply.github.com>
2022-12-11 12:35:02 -0500

    selectable card deck updates

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
    public Sprite[] cardFaces;
+   public GameObject cardPrefab;
+   public List<Card> deckList;
 
    public List<Card> generateDeck() {
         List<Card> deck = new List<Card>();
         Suit[] suits = new Suit[] {Suit.Club, Suit.Diamond, Suit.Heart, Suit.Spade};
         for (int s = 0; s < 4; s++) {
             for (int v = 0; v < 13; v++) {
-                deck.Add(new Card(id: s*13 + v +1, suit: suits[s], value: v+1, cardFaceSprite: cardFaces[s*13 + v]));
+                deck.Add(new Card(id: s*13 + v +1, suit: suits[s], value: v+1, cardFaceSprite: cardFaces[s*13 + v], isFaceUp: true));
             }
         }
         return deck;
    }
+
+   public void buildDeck(List<Card> deck) {
+        foreach(Card card in deck) {
+            GameObject newCard = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+            newCard.name = card.getId().ToString();
+        }
+   }
+        
+
+    void Awake() {
+
+        deckList = generateDeck();
+        buildDeck(deckList);
+
+    }
+
+   void Start()
+    {
+        
+        Shuffle.shuffleDeck(deckList);
+
+        
+    }
     
 }
--- Assets/Scripts/UpdateSprite.cs
+++ Assets/Scripts/UpdateSprite.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UpdateSprite : MonoBehaviour
 {
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private Deck deckScript;
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+
     // Start is called before the first frame update
     void Start()
     {
+        deckScript = FindObjectOfType<Deck>();
+        cardFace = deckScript.cardFaces[Int32.Parse(this.name)-1];
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (selectable.isFaceUp == true){
+            spriteRenderer.sprite = cardFace;
+        } else {
+            spriteRenderer.sprite = cardBack;
+        }
     }
 }
